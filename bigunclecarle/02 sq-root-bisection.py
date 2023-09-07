7bb524db924c33ce779db01f89963aaa94fda757
CJ Boris <33590307+jaunito76@users.noreply.github.com>
2023-09-07 16:33:46 -0500

    Homework Week 3

---
+++ bigunclecarle/02 sq-root-bisection.py
@@ -0,0 +1,4 @@
+'''
+This program will modify your previous square root algorithm 
+by using a bisection search in Chap 3 of your textbook. 
+'''
---
+++ saintmaker99/03 guess-number-bisection.py
@@ -0,0 +1,11 @@
+'''
+This program will allow the user to come up with a number in his or her
+head and then the computer will try to guess the number using a bisection
+search. The user will enter higher ('h'), lower ('l'), or exact ('e'). 
+
+The user will guess between 0 and 100 and the computer will only guess 
+whole numbers (no decimals). The computer will print out the guess on 
+each iteration, and then at the end it will print out:
+'Your answer is: ' <the number found>
+'The computer took <number of guesses> guess(es) to find your number'
+'''
