eecd8cd5745c35c5f25ce504e2adbf041b4b6779
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-09-08 12:20:24 -0500

    Homework Take 2

--- saintmaker99/sqguesser.py
+++ saintmaker99/sqguesser.py
@@ -1,1 +1,13 @@
 #Type your homework here
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+g=0
+f=float(input('Enter a float or int: '))
+while (g*g)<f:
+    g=g+0.00001
+print(g)
