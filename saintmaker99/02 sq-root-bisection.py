33e34375fffd2c4365ec47a146b042a76547719a
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-09-08 12:18:48 -0500

    Update homework week 3

--- saintmaker99/02 sq-root-bisection.py
+++ saintmaker99/02 sq-root-bisection.py
@@ -2,3 +2,22 @@
 This program will modify your previous square root algorithm 
 by using a bisection search in Chap 3 of your textbook. 
 '''
+num_geussses=0
+x=0
+epsilon=0.01
+low=0
+high=max(1, x)
+ans=(high+low)/2
+f=float(input('Enter a float or int for a suare root please:'))
+high=f
+while abs(ans**2-x)>=epsilon:
+    print('low=', low, 'high=', high, 'ans=', ans)
+    num_geussses+=1
+    if ans**2<x:
+        low=ans
+
+    else:
+        high=ans
+    ans=(high+low)/2
+print(num_geussses)
+print(ans)
--- saintmaker99/sqguesser.py
+++ saintmaker99/sqguesser.py
@@ -1,1 +1,15 @@
 #Type your homework here
+g=0
+f=float(input('Enter a float or int for a suare root (or kindly jump off a cliff please): '))
+while (g**3)<f:
+    g=g+0.00000001
+    print(g)
+print(g)
+n=float(input('enter a number to count:'))
+m=0
+while m<n:
+    m=m+1
+    print(m)
+print(m)
+x=float(input('enter a number to primetize:'))
+p=2
