38d3dedba4744ed0d6f91911463c3d70903f8f5f
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-09-11 14:06:50 -0500

    updated square root

--- saintmaker99/02 sq-root-bisection.py
+++ saintmaker99/02 sq-root-bisection.py
@@ -3,21 +3,20 @@ This program will modify your previous square root algorithm
 by using a bisection search in Chap 3 of your textbook. 
 '''
 num_geussses=0
-x=0
-epsilon=0.01
+epsilon=0.00001
 low=0
-high=max(1, x)
-ans=(high+low)/2
 f=float(input('Enter a float or int for a suare root please:'))
-high=f
-while abs(ans**2-x)>=epsilon:
+high=max(1, f)
+ans=(high+low)/2
+while abs(ans**2-f)>=epsilon:
     print('low=', low, 'high=', high, 'ans=', ans)
     num_geussses+=1
-    if ans**2<x:
+    if ans**2<f:
         low=ans
-
     else:
         high=ans
     ans=(high+low)/2
-print(num_geussses)
-print(ans)
+print('number of guesses',num_geussses)
+#print(num_geussses)
+print('answer',ans)
+#print(ans)
