9077af64de7dd4dac603d826a738f5387c5fde1f
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-09-26 22:10:02 -0500

    nvm

--- saintmaker99/02 sq-root-bisection copy.py
+++ saintmaker99/02 sq-root-bisection copy.py
@@ -3,10 +3,13 @@ This program will modify your previous square root algorithm
 by using a bisection search in Chap 3 of your textbook. 
 '''
 def sqr(f):
-    x=0
-    while (x*x)<f:
-        x=x + 0.001
-        print(x)
+    epsilon=0.01
+    k=float(input('Enter a float or int for a square root please:'))
+    guess=k/2
+    while abs(guess**2-k)>=epsilon:
+        guess=guess-(((guess**2)-k)/(2*guess))
+    print('square root of', k, 'is about', guess)
+    return guess
 print(sqr(1))
 f=float(input('Enter a float or int for a square root please:'))
 sqr(f)
