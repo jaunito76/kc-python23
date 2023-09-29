300e645331d86daa52fafbd9c808aed966d72f6b
jaunito76 <33590307+jaunito76@users.noreply.github.com>
2023-09-28 19:17:35 -0500

    Update

--- theswimminggrandpa/hangman/ps3_hangman.py
+++ theswimminggrandpa/hangman/ps3_hangman.py
@@ -110,5 +110,5 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
+secretWord = chooseWord(wordlist).lower()
 # hangman(secretWord)
