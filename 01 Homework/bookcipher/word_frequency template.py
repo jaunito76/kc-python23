dd4ed07fee5dbe642a7c9cffb41ac42d15f8a63e
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-11-06 20:51:16 -0600

    llllllllllllllllllllllllllllllllllllllllllllllllllllll

--- 01 Homework/bookcipher/word_frequency template.py
+++ 01 Homework/bookcipher/word_frequency template.py
@@ -1,5 +1,7 @@
 BOOK = 'don-quixote.txt'
 
+
+
 def word_frequency(file_name: str) -> dict:
     """
     file_name (str): this is the name 
@@ -8,6 +10,23 @@ def word_frequency(file_name: str) -> dict:
         {<WORD: str>: <count_of_word: int>} that contains the 
         number of times a word is used in the file above
     """
+
+    def get_list_from_file(file_name: str) -> list:
+        file = open(file_name)
+        with file:
+            
+
+    #   Open the file
+    #   read the file
+    #   close the file
+    #   Make the list of words from the file
+    #   Loop through all the words
+    #       Count each word 
+    #       If I've seen the word ->
+    #           add to the dictionary value
+    #       Else
+    #           Add word (key) to the dictionary
+    #   Return dict
 
 def sort_by_frequency(frequency):
     """
