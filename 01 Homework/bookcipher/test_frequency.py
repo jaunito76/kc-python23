43637c3e7ab3e14ac1924572f19ba4c60cd2e357
Saint Maker <daniel.j.kliethermes@gmail.com>
2023-11-13 08:44:24 -0600

    whoo

--- 01 Homework/bookcipher/test_frequency.py
+++ 01 Homework/bookcipher/test_frequency.py
@@ -1,23 +1,24 @@
 import pytest
-from word_frequency import word_frequency, sort_by_frequency
+import word_frequency as wf
 
 def test_empty_file():
-    assert word_frequency('empty.txt') == {}
+    assert wf.word_frequency('empty.txt') == {}
 
 def test_single_word():
-    assert word_frequency('single_word.txt') == {'Python': 1}
+    assert wf.word_frequency('single_word.txt') == {'python': 1}
 
 def test_repeated_words():
-    assert word_frequency('repeated_words.txt') == {'Python': 3}
+    assert wf.word_frequency('repeated_words.txt') == {'python': 3}
 
 def test_multiple_words():
-    assert word_frequency('multiple_words.txt') == {'Python': 1, 'is': 1, 'great': 1}
+    assert wf.word_frequency('multiple_words.txt') == {'python': 1, 'is': 1, 'great': 1}
 
 def test_punctuation():
-    assert word_frequency('punctuation.txt') == {'Python': 1, 'is': 1, 'great': 1}
+    assert wf.word_frequency('punctuation.txt') == {'python': 1, 'is': 1, 'great': 1}
 
 def test_large_file():
-    frequency = word_frequency('large_file.txt')
-    sorted_frequency = sort_by_frequency(frequency)
+    frequency = wf.word_frequency('large_file.txt')
+    assert frequency == {'the':1000, 'and':500, 'a':301}
+    sorted_frequency = wf.sort_by_frequency(frequency)
     # Check the first few words and their frequencies
     assert sorted_frequency[:3] == [('the', 1000), ('and', 500), ('a', 300)]
